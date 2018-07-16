e075481255b0cdeb36dfb02721ce4a903f23da9f
Bryan Chen <bryan@Bryans-MacBook-Pro.local>
2018-07-16 12:19:02 +0800

    Fix command in extension.js

--- extension.js
+++ extension.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 // your extension is activated the very first time the command is executed
 function activate(context) {
   let disposable = vscode.commands.registerCommand(
-    "openRelatedFile.open",
+    "openRelatedFiles.open",
     function() {
       let config = vscode.workspace.getConfiguration("openRelatedFile");
       let ignoreExtArr = [...config.get("ignoreExt")];
